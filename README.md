12a405eae2d063f5c090eed914d9f1c2ee3d8ce2
Niemistoo <jappe.nie@gmail.com>
2024-10-04 17:04:14 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Discord-Bot-SPOT
+
+Script for automating electricity price service that sends expensive SPOT price hours to my Discord channel.
+
+Scheduler runs on cloud service and runs the task every morning 07:00AM.
---
+++ main.js
@@ -0,0 +1,83 @@
+const dotenv = require('dotenv');
+
+dotenv.config('./.env');
+
+const WEBHOOK_URL = process.env.WEBHOOK_URL;
+const API_URL = process.env.API_URL;
+
+// Fetch the electricity prices from the API
+async function fetchElectricityPrices() {
+    try {
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        data.prices.reverse();
+        return data;
+
+    } catch (error) {
+        console.error('There was a problem fetching the electricity prices:', error);
+    }
+}
+
+// Get the high prices of the day in a string format
+async function getHighPricesString(data) {
+    let dateToday = new Date().toLocaleDateString();
+    let pricesToday = [];
+    data.prices.forEach(content => {
+        let date = new Date(content.startDate).toLocaleDateString();
+        if (date === dateToday) {
+            if (content.price > 9) {
+                let time = new Date(content.startDate).toLocaleTimeString('fi-FI', {
+                    hour: '2-digit',
+                    minute: '2-digit'
+                });
+                let price = new Intl.NumberFormat('fi-FI', {
+                    maximumFractionDigits: 2,
+                    maximumFractionDigits: 2
+                }).format(content.price);
+                pricesToday.push({
+                    time: time,
+                    price: price
+
+                });
+            }
+        }
+    });
+    const finalString = pricesToday.map(price => `klo: ${price.time}, hinta: ${price.price}snt/kWh`).join('\n\n');
+    return finalString;
+}
+
+// Send a message to the Discord channel
+async function alertHighPrices(data) {
+    const message = {
+        "content": data,
+        "username": 'Price Bot'
+    };
+
+    const response = await fetch(WEBHOOK_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message)
+    });
+
+    if (response.ok) {
+        console.log('Message sent successfully!');
+    } else {
+        console.error('Error sending message:', response.statusText);
+    }
+}
+
+async function app() {
+    const data = await fetchElectricityPrices();
+    const highPricesString = await getHighPricesString(data);
+
+    if (highPricesString.length > 0) {
+        alertHighPrices(highPricesString);
+    }
+}
+
+app();
